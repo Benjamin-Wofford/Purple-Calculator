e60203e0be91241970a35774b2033b3a48a62bdb
Critter4Dinner <woffordben1992@gmail.com>
2019-12-05 18:20:47 -0500

    DRYed out the folder structure by combining individual number buttons into a single component called Keypad

---
+++ src/components/CalculatorBase.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+class CalculatorBase extends React.Component{
+
+    render(){
+        return (
+            <main>
+                
+            </main>
+        )
+            
+    }
+}
