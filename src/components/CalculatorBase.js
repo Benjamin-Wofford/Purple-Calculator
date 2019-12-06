0e7bc4495c7aad1f964420c275d4574290067bc0
Critter4Dinner <woffordben1992@gmail.com>
2019-12-06 10:09:18 -0500

    Added all the button components to the calculator base

--- src/components/CalculatorBase.js
+++ src/components/CalculatorBase.js
@@ -1,12 +1,44 @@
 import React from 'react';
-
+import AdditionButton from './AdditionButton';
+import ClearButton from './ClearButton';
+import DecimalButton from './DecimalButton';
+import DivisionButton from './DivisionButton';
+import EightButton from './EightButton';
+import EqualsButton from './EqualsButton';
+import FiveButton from './FiveButton';
+import FourButton from './FourButton';
+import NineButton from './NineButton';
+import OneButton from './OneButton';
+import OutputDisplay from './OutputDisplay';
+import SevenButton from './SevenButton';
+import SixButton from './SixButton';
+import SubtractionButton from './SubtractionButton';
+import ThreeButton from './ThreeButton';
+import TwoButton from './TwoButton';
+import ZeroButton from './ZeroButton';
 
 class CalculatorBase extends React.Component{
 
     render(){
         return (
             <main className="baseboard">
-
+                <OutputDisplay/>
+                <ClearButton/>
+                <DivisionButton/>
+                <SevenButton/>
+                <EightButton/>
+                <NineButton/>
+                <SubtractionButton/>
+                <FourButton/>
+                <FiveButton/>
+                <SixButton/>
+                <AdditionButton/>
+                <OneButton/>
+                <TwoButton/>
+                <ThreeButton/>
+                <EqualsButton/>
+                <DecimalButton/>
+                <ZeroButton/>
             </main>
         )
             
