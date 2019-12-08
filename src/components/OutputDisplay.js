66763348d4a7678e17a421d528657234e51c48b0
Critter4Dinner <woffordben1992@gmail.com>
2019-12-08 11:12:16 -0500

    cleaned up the directory folder

--- src/components/OutputDisplay.js
+++ src/components/OutputDisplay.js
@@ -2,7 +2,8 @@ import React from 'react';
 
 
 class OutputDisplay extends React.Component{
-
+    
+// this needs to have state that I can change and then output the state to the span
 
     render(){
         return <p className='output'><span id='answer'>12</span></p>
