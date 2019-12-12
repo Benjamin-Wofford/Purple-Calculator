704f8b92f1aa0fcfa613079a05d4ce07f12a94f1
Critter4Dinner <woffordben1992@gmail.com>
2019-12-12 05:23:53 -0500

    implemented fittext on calculator make excessively large numbers shrink down in size instead of trailing offscreen. Not functioning at the moment.

--- src/components/OutputDisplay.js
+++ src/components/OutputDisplay.js
@@ -1,11 +1,11 @@
 import React from 'react';
-
+import FitText from '@kennethormandy/react-fittext'
 
 class OutputDisplay extends React.Component{
     
 
     render(props){
-        return <p className='output'><span id='answer'>{this.props.placeThisOnScreen}</span></p>
+        return <p className='output'><span id='answer'><FitText compressor={0.1}>{this.props.placeThisOnScreen}</FitText></span></p>
     }
 }
 
