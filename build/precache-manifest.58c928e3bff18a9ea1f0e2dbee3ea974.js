f68224b5569061d571c6756a8a44231ad60252cb
Critter4Dinner <woffordben1992@gmail.com>
2019-12-12 09:20:10 -0500

    fixed the git ignore file so hopefully the gh page will display properly

---
+++ build/precache-manifest.58c928e3bff18a9ea1f0e2dbee3ea974.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4dd152a0e4d2d32702084116bbab7dbe",
+    "url": "/purple-calculator/index.html"
+  },
+  {
+    "revision": "ef61edba45d524e4b8f6",
+    "url": "/purple-calculator/static/css/main.44041ac0.chunk.css"
+  },
+  {
+    "revision": "07fe38795bc28a3af776",
+    "url": "/purple-calculator/static/js/2.23f90349.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/purple-calculator/static/js/2.23f90349.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ef61edba45d524e4b8f6",
+    "url": "/purple-calculator/static/js/main.ace6ce1f.chunk.js"
+  },
+  {
+    "revision": "52fcf1fedbef616b0545",
+    "url": "/purple-calculator/static/js/runtime-main.599156da.js"
+  }
+]);
